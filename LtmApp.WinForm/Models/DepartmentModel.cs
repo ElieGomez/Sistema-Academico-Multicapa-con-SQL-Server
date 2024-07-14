3222bd28d60c5875b1021776b93967b10c9c8ce5
MSI GL65 <MSI GL65@MSI>
2024-07-14 17:19:45 -0400

    first commit

---
+++ LtmApp.WinForm/Models/DepartmentModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using LtmApp.WinForm.Exceptions;
+
+namespace LtmApp.WinForm.Models
+{
+    public class DepartmentModel
+    {
+        private int departmentId;
+        public int DepartmentId
+        {
+            get { return this.departmentId; }
+            set
+            {
+                if ((value) == null)
+                {
+                    throw new StudentException("El campo Id es requerido");
+                }
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new StudentException("El campo name es requerido");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new StudentException("La longitud del name es invalida");
+                }
+            }
+        }
+
+        private int budget;
+        public int Budget
+        {
+            get { return this.budget; }
+            set
+            {
+                if ((value) == null)
+                {
+                    throw new StudentException("El campo budget es requerido");
+                }
+            }
+        }
+
+        private DateTime startDate;
+        public DateTime StartDate
+        {
+            get { return this.startDate; }
+            set
+            {
+                if ((value) == null)
+                {
+                    throw new StudentException("El campo Id es requerido");
+                }
+            }
+        }
+    }
+}
---
+++ LtmApp.WinForm/Models/PersonModel.cs
@@ -0,0 +1,40 @@
+﻿using LtmApp.WinForm.Exceptions;
+
+namespace LtmApp.WinForm.Models
+{
+    public class PersonModel
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new StudentException("El campo nombre es requerido");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new StudentException("La longitud del nombre es invalida");
+                }
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new StudentException("El campo nombre es requerido");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new StudentException("La longitud del nombre es invalida");
+                }
+            }
+        }
+    }
+}
